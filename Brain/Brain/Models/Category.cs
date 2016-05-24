41d4023d504fef694dbbfb4bc11564a5d2ad7322
Bonnev <piggov@gmail.com>
2016-05-24 16:54:45 +0300

    Added ShowActivities View and Controller and Category Classes

---
+++ Brain/Brain/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Brain.Models
+{
+    public class Category : CategoryElement
+    {
+        public Category ParentElement { get; private set; }
+        public List<CategoryElement> Elements { get; set; }
+
+        public Category(string title, Category parent) : base(title)
+        {
+            Elements = new List<CategoryElement>();
+            ParentElement = parent;
+        }
+
+
+        public void AddElement(CategoryElement element)
+        {
+            Elements.Add(element);
+        }
+    }
+}
